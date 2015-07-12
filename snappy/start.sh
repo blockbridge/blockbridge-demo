205234c472e1cf84b06ca2b3f86a699d10d86f65
Stephen Rust <srust@blockbridge.com>
2015-07-12 00:37:28 -0400

    Fixup container names for snappy scripts

--- snappy/start.sh
+++ snappy/start.sh
@@ -8,8 +8,8 @@ VAL=1
 if [ $# -ne 0 ]; then
     VAL=$1
 fi
-APP="mongo-app-$VAL"
-VOL="snappy-$VAL"
+APP="mongo-snap-app-$VAL"
+VOL="mongo-snap-vol-$VAL"
 
 # start Blockbridge persistent storage volume
 echo -n "Starting persistent storage container $VOL: "
--- snappy/stop.sh
+++ snappy/stop.sh
@@ -3,8 +3,8 @@ VAL=1
 if [ $# -ne 0 ]; then
     VAL=$1
 fi
-APP="mongo-app-$VAL"
-VOL="snappy-$VAL"
+APP="mongo-snap-app-$VAL"
+VOL="mongo-snap-vol-$VAL"
 
 echo -n "Stopping $APP: "
 docker stop $APP
